b9e11c712fb7f27af27469464fa7a51134701af2
Mohammad Hossein <ho3ein2532@gmail.com>
2022-01-15 19:49:01 +0330

    update

--- Jira/src/main/java/models/DatabaseHandler.java
+++ Jira/src/main/java/models/DatabaseHandler.java
@@ -65,6 +65,22 @@ public class DatabaseHandler {
         return bool;
     }
 
+    public static boolean doesTeamExist(String teamName, String username) throws SQLException {
+        
+        String sql = String.format(Queries.DOES_TEAM_EXIST_FOR_USER, teamName);
+        connect();
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        if (result.next()) {
+            String leader = result.getString(1);
+
+        }else {
+        }
+        statement.close();
+        connection.close();
+        return bool;
+    }
+
     public static boolean doesEmailExist(String email) throws SQLException {
         String sql = String.format(Queries.DOES_Email_EXIST, email);
         connect();
--- Jira/src/main/java/view/MainMenu.java
+++ Jira/src/main/java/view/MainMenu.java
@@ -7,23 +7,32 @@ import java.util.regex.Matcher;
 
 public class MainMenu {
     public static void execute(String command) throws SQLException {
-        Matcher matcher = Regex.getCommandMatcher(command, "^enter\\smenu\\s(.+)$");
-        if (matcher.find()) {
-            String menu = matcher.group(1);
+        Matcher showTeamsMatcher = Regex.getCommandMatcher(command, "^enter\\smenu\\s(.+)$");
+        Matcher showTeamMatcher = Regex.getCommandMatcher(command, "^show\\s--team\\s(.+)$");
+        Matcher createTeam = Regex.getCommandMatcher(command, "create\\s--team\\s(.+)");
+        if (showTeamsMatcher.find()) {
+            String menu = showTeamsMatcher.group(1);
             switch (menu){
                 case "profile menu":
+                    MenuController.currentMenu = Menus.PROFILE_MENU;
                     break;
                 case "team menu":
+                    MenuController.currentMenu = Menus.TEAM_MENU;
                     break;
                 case "tasks page":
+                    MenuController.currentMenu = Menus.TASKS_PAGE;
                     break;
                 case "calendar menu":
+                    MenuController.currentMenu = Menus.CALENDAR_MENU;
                     break;
                 case "notification bar":
+                    MenuController.currentMenu = Menus.NOTIFICATION_BAR;
                     break;
             }
         } else if(command.equals("show --teams")) {
             MainMenuController.showTeams();
+        } else if (showTeamMatcher.find()) {
+            MainMenuController.showTeam(showTeamMatcher.group(1));
         }
 
     }
