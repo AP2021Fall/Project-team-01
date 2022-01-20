1e1b2e21c27671acab189d77cbe35d3fc8b335fa
Mohammad Hossein <ho3ein2532@gmail.com>
2022-01-21 01:28:31 +0330

    update sql handler

--- Jira/src/main/java/controller/TeamMenuController/ScoreBoardController.java
+++ Jira/src/main/java/controller/TeamMenuController/ScoreBoardController.java
@@ -2,14 +2,15 @@ package controller.TeamMenuController;
 
 import models.DatabaseHandler;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ScoreBoardController {
 
-    public static void showScoreboard() {
+    public static void showScoreboard() throws SQLException {
         ArrayList<String> show = DatabaseHandler.showScoreboard(TeamMenuController.getTeam().getName());
         for (int i = 0; i < show.size();i++)
-            System.out.println(i + " " + show.get(i));
+            System.out.println(show.get(i));
     }
 
     public static void showMenu(){
--- Jira/src/main/java/models/Queries.java
+++ Jira/src/main/java/models/Queries.java
@@ -82,5 +82,20 @@ public class Queries {
     public static final String DOES_TASK_EXIST =
             "SELECT id FROM tasks WHERE id = %d";
     public static final String GET_TASK_LEADER_BY_TASK_ID =
-            "";
+            "SELECT leader FROM tasks JOIN teams ON tasks.team_id = teams.id WHERE id = %d";
+
+    //change task options
+    public static final String UPDATE_PRIORITY =
+            "UPDATE tasks SET priority = '%s' WHERE id = %d";
+    public static final String UPDATE_DESCRIPTION =
+            "UPDATE tasks SET description = '%s' WHERE id = %d";
+    public static final String UPDATE_TITLE =
+            "UPDATE tasks SET title = '%s' WHERE id = %d";
+
+    public static final String ADD_LOGS =
+            "UPDATE users SET logs = '%s' WHERE username = '%s'";
+
+    public static final String ROAD_MAP =
+            "SELECT GREATEST(LEAST(100*(now() - `creation date`)/(`deadline date` - `creation date`), 100),0) AS pecent FROM tasks WHERE team_id = %d ORDER BY `creation date` ASC, title";
+
 }
