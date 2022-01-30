4a24b047345aac8d603d0a5caf73868487aaf849
Mohammad Hossein <ho3ein2532@gmail.com>
2022-01-31 00:27:26 +0330

    graphic

--- Jira/src/main/java/view/ProfileMenu/ProfileMenuGraphic.java
+++ Jira/src/main/java/view/ProfileMenu/ProfileMenuGraphic.java
@@ -2,10 +2,10 @@ package view.ProfileMenu;
 
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
+import view.MenusFxml;
 import view.SceneController;
 
 public class ProfileMenuGraphic {
-    public static Stage stage;
     public SceneController sceneController = new SceneController();
 
     public void showTeams(ActionEvent actionEvent) {
@@ -13,17 +13,22 @@ public class ProfileMenuGraphic {
     }
 
     public void showMyProfile(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.SHOW_MY_PROFILE.getLabel());
     }
 
     public void changeUsername(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.CHANGE_USERNAME_MENU.getLabel());
     }
 
     public void changePassword(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.CHANGE_PASSWORD_MENU.getLabel());
     }
 
     public void showLogs(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.SHOW_LOGS_MENU.getLabel());
     }
 
     public void showNotifications(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.SHOW_NOTIFICATION_MENU.getLabel());
     }
 }
