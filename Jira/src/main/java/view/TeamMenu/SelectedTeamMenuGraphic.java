316f578cd9e0544d73fb11e5398c0150f820a266
Ghazalthn <ghazallthn@gmail.com>
2022-01-31 22:40:44 +0330

    teamMenu graphic

--- Jira/src/main/java/view/TeamMenu/SelectedTeamMenuGraphic.java
+++ Jira/src/main/java/view/TeamMenu/SelectedTeamMenuGraphic.java
@@ -1,21 +1,24 @@
 package view.TeamMenu;
 
-import controller.ProfileMenuController.ProfileMenuController;
+import controller.TeamMenuController.TeamMenuController;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import view.MenusFxml;
+import view.SceneController;
 
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class SelectedTeamMenuGraphic implements Initializable {
-    public TextArea textTeamMembers;
+    public SceneController sceneController = new SceneController();
+
+    public Label welcome;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //TODO
-        textTeamMembers.setText("members");
+        welcome.setText("Team: "+ TeamMenuController.getTeam().getName());
     }
 
     public void goToRoadMap(ActionEvent actionEvent) {
@@ -28,9 +31,13 @@ public class SelectedTeamMenuGraphic implements Initializable {
     }
 
     public void goToMainMenu(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.MEMBER_MAIN_MENU.getLabel());
     }
 
     public void goToScoreBoard(ActionEvent actionEvent) {
+    }
 
+    public void backToTeamMenu(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.TEAM_MENU.getLabel());
     }
 }
