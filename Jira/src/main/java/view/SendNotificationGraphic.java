835b7c85dc82d88ff8bdf5e0a856c1753a2f9fcc
Mohammad Hossein <ho3ein2532@gmail.com>
2022-02-04 20:43:07 +0330

    graphic

---
+++ Jira/src/main/java/view/SendNotificationGraphic.java
@@ -0,0 +1,38 @@
+package view;
+
+import controller.MainMenuController;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+
+public class SendNotificationGraphic {
+    public TextField notification;
+    public SceneController sceneController = new SceneController();
+
+    public void send(ActionEvent actionEvent) throws SQLException {
+        String notifications = notification.getText();
+        if (notifications.isEmpty())
+            return;
+        switch (MainMenuController.choice) {
+            case 1 :
+                MainMenuController.sendNotificationToUser(notifications, MainMenuController.username);
+                sceneController.switchScene(MenusFxml.LEADER_MAIN_MENU.getLabel());
+                break;
+            case 2:
+                MainMenuController.sendNotificationToTeam(notifications, MainMenuController.team);
+                sceneController.switchScene(MenusFxml.LEADER_MAIN_MENU.getLabel());
+                break;
+            case 3:
+                MainMenuController.sendNotificationToAll(notifications);
+                sceneController.switchScene(MenusFxml.LEADER_MAIN_MENU.getLabel());
+                break;
+            default:
+                return;
+        }
+    }
+
+    public void goToMainMenu(ActionEvent actionEvent) {
+        sceneController.switchScene(MenusFxml.LEADER_MAIN_MENU.getLabel());
+    }
+}
