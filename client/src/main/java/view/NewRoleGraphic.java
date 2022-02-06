8b6c5b534286fbf6531c4ee701607b36c3d127fa
Mohammad Hossein <ho3ein2532@gmail.com>
2022-02-06 19:32:42 +0330

    network

--- client/src/main/java/view/NewRoleGraphic.java
+++ client/src/main/java/view/NewRoleGraphic.java
@@ -1,11 +1,14 @@
 package view;
 
+import appController.AppController;
 import controller.MainMenuController;
 import controller.TeamMenuController.ScoreBoardController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import models.User;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class NewRoleGraphic {
@@ -17,8 +20,9 @@ public class NewRoleGraphic {
         sceneController.switchScene(MenusFxml.USERS_OPTIONS.getLabel());
     }
 
-    public void next(ActionEvent actionEvent) throws SQLException {
+    public void next(ActionEvent actionEvent) throws SQLException, IOException {
         String choice = (String) choiceBox.getValue();
+        AppController.getOutputStream().writeUTF("changeRole1 " + choice + " " + User.getToken());
         String result = MainMenuController.changeRole(ScoreBoardController.usernameToRemove, choice);
         alert.setText(result);
         if (result.equals("now enter a username to replace with this leader in team")) {
